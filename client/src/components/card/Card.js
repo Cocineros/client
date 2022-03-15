3a98793e4d6cf86eb730ea1adb9c42694c40b3c8
Ayse Jones <aysej305@gmail.com>
2022-03-15 15:24:55 -0500

    card updated with button

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -5,21 +5,26 @@ export default function Card() {
         <div className="card-container">
             <h1>card example</h1>
             <div className="recipe-card">
-            <h2 id="add-recipe">Add a recipe</h2>
-            <br></br>
-            <h3>Dish name:</h3>
-            <input placeholder= "Enter dish name here"></input>
-            <input placeholder= "Enter time to make dish here"></input>
-            <br></br>
-            <div className='recipe-detail-container'>
-                <h3>Ingredients:</h3>
-                <input placeholder= "Enter ingredients here"></input>
-                <h3>Directions:</h3>
-                <input placeholder= "Enter directions here"></input>
-                <h3>Upload image:</h3>
-                <input placeholder= "Optional: upload image of dish here"></input>
+                <h2 id="add-recipe">Add a recipe</h2>
+                <br></br>
+                <div className='dish-details'>
+                    <h3>Dish name:</h3>
+                    <input placeholder="Enter dish name here"></input>
+                    <h3>Dish time:</h3>
+                    <input placeholder="Enter time to make dish here"></input>
+                </div>
+                <br></br>
+                <div className='recipe-detail-container'>
+                    <h3>Ingredients:</h3>
+                    <input placeholder="Enter ingredients here"></input>
+                    <h3>Directions:</h3>
+                    <input placeholder="Enter directions here"></input>
+                    <h3>Upload image:</h3>
+                    <input placeholder="Optional: upload image of dish here"></input>
+                </div>
+                <br></br>
+                <button>add recipe to cookbook</button>
             </div>
-        </div>
         </div>
     )
 }
