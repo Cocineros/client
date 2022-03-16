77329a755198238fb94812284f01d2d9eeda0583
amadeamargo <amadeamargo@gmail.com>
2022-03-15 20:58:59 -0500

    added recipe component

---
+++ client/src/components/recipe/Recipe.js
@@ -0,0 +1,27 @@
+import './recipe.css'
+
+export default function Recipe () {
+    return (  
+        <>
+        <div id="recipe-page">
+        <div id="dish-name-container">
+            <h3 id="dish-name">dish name</h3>
+        </div>
+        <div id="image-contianer">
+            image here
+        </div>
+        <div id="ingredients-container">
+            ingredients container
+        </div>
+        <div id="directions-container">
+            directions container
+        </div>
+
+
+        </div>
+
+        </>
+
+    )
+}
+
