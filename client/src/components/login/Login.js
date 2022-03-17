b1be10b7e13f40778a09da1a1cfc4881c0643a75
Brian Garland <briangarlandk@gmail.com>
2022-03-16 19:40:19 -0500

    Fixing login and signup

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,20 +1,64 @@
+import React, { useState } from 'react';
 import './login.css'
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_PROFILE } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
 
 export default function Login() {
+    const [profileLogin, setProfileLogin] = useState({ email: '', password: '' });
+    const [login, { error, data }] = useMutation(LOGIN_PROFILE);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setProfileLogin({
+            ...profileLogin,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(profileLogin)
+
+        try {
+            const { data } = await login({
+                variables: { ...profileLogin },
+            });
+
+            Auth.login(data.login.token)
+        } catch (e) {
+            console.error(e)
+        }
+
+        setProfileLogin({
+            email: '',
+            password: '',
+        });
+    };
+
     return (
         <>
             {/* <div className="login"> */}
-
-                {/* <h2>Log in to myCookbook</h2> */}
-
-                <form className="login-form">
+            {/* <h2>Log in to myCookbook</h2> */}
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+                <form className="login-form" onSubmit={handleFormSubmit}>
                     <div className="form-group">
-                        <input placeholder="Email" type="email" id="email" required="true" />
+                        <input placeholder="Email" name="email" type="email" id="email" required="true" value={profileLogin.email} onChange={handleChange} />
                     </div>
                     <div className="form-group">
-                        <input placeholder="Password" type="text" id="password" required="true" />
+                        <input placeholder="Password" name="password" type="password" id="password" required="true" value={profileLogin.password} onChange={handleChange} />
                     </div>
                 </form>
+            )}
 
                 <h3>Don't have an account? <a href="/signup">Sign up</a></h3>
             {/* </div> */}
--- client/src/components/signup/Signup.js
+++ client/src/components/signup/Signup.js
@@ -7,27 +7,27 @@ import { ADD_PROFILE } from '../utils/mutations'
 import Auth from '../utils/auth'
 
 export default function Signup() {
-    const [profileSignup, setProfileSignup] = useState({
-        firstName: '',
-        lastName: '',
-        username: '',
-        email: '',
-        password: '',
+  const [profileSignup, setProfileSignup] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
+
+  const handleChange = (event) => {
+    console.log(event)
+    const { name, value } = event.target;
+
+    setProfileSignup({
+      ...profileSignup,
+      [name]: value,
     });
+  };
 
-    const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
-
-    const handleChange = (event) => {
-        console.log(event)
-        const { name, value } = event.target;
-
-        setProfileSignup({
-            ...profileSignup,
-            [name]: value,
-        });
-    };
-
-    // submit form
+  // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -40,77 +40,85 @@ export default function Signup() {
     } catch (e) {
       console.error(e);
     }
-  }; 
+
+    setProfileSignup({
+      firstName: '',
+      lastName: '',
+      username: '',
+      email: '',
+      password: '',
+    })
+  };
 
 
-    return (
-        <>
-            <div> 
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/dashboard">back to the homepage.</Link>
-              </p>
-            ) : (
-            <form className="signup-form " onSubmit={handleFormSubmit}>
-                    <input placeholder="First Name" 
-                    type="text" 
-                    name="firstName" 
-                    id="fname" 
-                    required="true" 
-                    value={profileSignup.firstName}
-                    onChange={handleChange} 
-                     />
+  return (
+    <>
+      <div>
+        {data ? (
+          <p>
+            Success! You may now head{' '}
+            <Link to="/">back to the homepage.</Link>
+          </p>
+        ) : (
+          <form className="signup-form " onSubmit={handleFormSubmit}>
+            <input placeholder="First Name"
+              type="text"
+              name="firstName"
+              id="fname"
+              required
+              value={profileSignup.firstName}
+              onChange={handleChange}
+            />
 
-                    <input placeholder="Last Name" 
-                    type="text" 
-                    name="lastName" 
-                    id="lname"
-                    required="true" 
-                    value={profileSignup.lastName}
-                    onChange={handleChange}
-                    />
+            <input placeholder="Last Name"
+              type="text"
+              name="lastName"
+              id="lname"
+              required
+              value={profileSignup.lastName}
+              onChange={handleChange}
+            />
 
-                    <input placeholder="Username" 
-                    type="username" 
-                    name="username" 
-                    id="username" 
-                    required="true" 
-                    value={profileSignup.username}
-                    onChange={handleChange} 
-                    />
+            <input placeholder="Username"
+              type="username"
+              name="username"
+              id="username"
+              required
+              value={profileSignup.username}
+              onChange={handleChange}
+            />
 
-                    <input placeholder="Email" 
-                    type="email" 
-                    name="email" 
-                    id="email" 
-                    required="true" 
-                    value={profileSignup.email}
-                    onChange={handleChange} 
-                    />
+            <input placeholder="Email"
+              type="email"
+              name="email"
+              id="email"
+              required
+              value={profileSignup.email}
+              onChange={handleChange}
+            />
 
-                    <input placeholder="Password" 
-                    type="password" 
-                    name="password" 
-                    id="password" 
-                    required="true" 
-                    value={profileSignup.password}
-                    onChange={handleChange} 
-                    />
+            <input placeholder="Password"
+              type="password"
+              name="password"
+              id="password"
+              required
+              value={profileSignup.password}
+              onChange={handleChange}
+            />
 
-                    <input placeholder="Confirm Password" 
-                    type="password" 
-                    name="confirmPassword" 
-                    id="confirm-password" 
-                    required="true" 
-                    onChange={handleChange}
-                     />
+            <input placeholder="Confirm Password"
+              type="password"
+              name="confirmPassword"
+              id="confirm-password"
+              required
+              onChange={handleChange}
+            />
 
-                    <button type="submit">Submit</button>
-            </form>
-            )}
-            <h3>Already have an account? <a href="/login">Log in</a></h3>
-            </div>
-        </>
-    )
+            <button type="submit">Submit</button>
+          </form>
+        )}
+        <h3>Already have an account? <a href="/login">Log in</a></h3>
+      </div>
+    </>
+  )
 }
